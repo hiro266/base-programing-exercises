6a79101ff83c9ee650b4e020296b44a3e94958d1
hiro266 <t1freejob@gmail.com>
2025-03-20 11:18:46 +0900

    update: No. 11 ごあいさつ10回

--- basis/no.11-ten-times-output-hello-world.js
+++ basis/no.11-ten-times-output-hello-world.js
@@ -1,5 +1,13 @@
 // Hello World!を10回繰り返して表示するプログラムを作成せよ。
 
-for (i = 0; i < 10; i++) {
+// for文
+// for (i = 0; i < 10; i++) {
+//   console.log("Hello World!");
+// }
+
+// while文
+let n = 0;
+while (n < 10) {
   console.log("Hello World!");
+  n++;
 }
