bc5e96735c590ec1154a9adc9d92038787e2bedc
hiro266 <t1freejob@gmail.com>
2025-03-21 22:46:18 +0900

    No. 19 配列に入力値を格納

---
+++ basis/no.19-store-input-value-to-array.js
@@ -0,0 +1,31 @@
+// 要素数5の整数型の配列を宣言し、すべての配列に対して順に入力された整数値を代入し、すべての要素の値を表示するプログラムを作成せよ。
+
+const utils = require("./utils/utils");
+const { rl, isInteger } = utils;
+
+rl.prompt();
+
+const arr = [...Array(5)];
+let currentCount = 0;
+let inputCount = arr.length - 1;
+
+rl.on("line", (input) => {
+  const convertedNumber = Number(input);
+
+  if (isInteger(convertedNumber)) {
+    console.log("整数を入力して下さい");
+    rl.close();
+    return;
+  }
+
+  if (currentCount >= inputCount) {
+    arr.splice(currentCount, 1, convertedNumber);
+    arr.map((i) => console.log(i));
+    rl.close();
+    return;
+  } else {
+    arr.splice(currentCount, 1, convertedNumber);
+    currentCount++;
+    rl.prompt();
+  }
+});
