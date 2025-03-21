bc61e65495792580c774c1a9d24308e4fde377f3
hiro266 <t1freejob@gmail.com>
2025-03-21 22:13:40 +0900

    No. 16 0でおしまい

---
+++ basis/no.16-end-at-zero.js
@@ -0,0 +1,25 @@
+// 整数値を入力させ、入力値が0でなければ再度入力させ、0であれば終了するプログラムを作成せよ。
+
+const utils = require("./utils/utils");
+const { rl, isInteger } = utils;
+
+rl.prompt();
+
+rl.on("line", (input) => {
+  const convertedNumber = Number(input);
+
+  if (isInteger(convertedNumber)) {
+    console.log("整数を入力して下さい");
+    rl.close();
+    return;
+  }
+
+  if (convertedNumber === 0) {
+    console.log(`input number: ${convertedNumber}`);
+    rl.close();
+    return;
+  } else {
+    console.log(`input number: ${convertedNumber}`);
+    rl.prompt(); // 次の入力へ
+  }
+});
