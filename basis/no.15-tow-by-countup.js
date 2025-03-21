e09a52b96826b28dacf136a422ff029411007c02
hiro266 <t1freejob@gmail.com>
2025-03-21 22:07:40 +0900

    No. 15 2ずつカウントアップ

---
+++ basis/no.15-tow-by-countup.js
@@ -0,0 +1,18 @@
+// 整数値を入力させ、0から入力値を超えない値まで2ずつ増やして表示するプログラムを作成せよ。
+const utils = require("./utils/utils");
+const { rl, isInteger } = utils;
+
+rl.question("整数を入力>", (input) => {
+  const convertedNumber = Number(input);
+  if (isInteger(convertedNumber)) {
+    console.log("整数を入力して下さい");
+    rl.close();
+    return;
+  }
+
+  for (i = 0; i <= convertedNumber; i += 2) {
+    console.log(i);
+  }
+
+  rl.close();
+});
