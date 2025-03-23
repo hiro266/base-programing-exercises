973e3258de37a9b603cc4ae7f14391eee8771cd2
hiro266 <t1freejob@gmail.com>
2025-03-23 12:12:16 +0900

    Rename beginner utils isInteger function to isNotInteger

--- beginner/no.21-gt5-and-lt20.js
+++ beginner/no.21-gt5-and-lt20.js
@@ -1,14 +1,14 @@
 // No. 21 5より大きく20より小さい
 // 整数値を入力させ、その値が5よりも大きく、かつ、20よりも小さければOKと表示するプログラムを作成せよ。
 
-const { rl, isInteger } = require("../utils/utils");
+const { rl, isNotInteger } = require("../utils/utils");
 
 rl.prompt();
 
 rl.on("line", (input) => {
   const number = Number(input.trim());
 
-  if (isInteger(number)) {
+  if (isNotInteger(number)) {
     console.log("整数を入力して下さい");
     rl.close();
     return;
