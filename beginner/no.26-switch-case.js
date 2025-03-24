63cee89e37417116ffdbfeb0a3cff5a04c39c9a6
hiro266 <t1freejob@gmail.com>
2025-03-24 22:28:45 +0900

    No. 26 switch-case

---
+++ beginner/no.26-switch-case.js
@@ -0,0 +1,32 @@
+// No. 26 switch-case
+// 整数値を入力させ、その値が1ならone、2ならtwo、3ならthree、それ以外ならothersと表示するプログラムをswicth-case文を使って作成せよ。
+
+const { rl, isNotInteger } = require("../utils/utils");
+
+const processInput = (input) => {
+  const number = Number(input.trim());
+
+  if (isNotInteger(number)) {
+    console.log("整数を入力して下さい");
+    rl.close();
+    return;
+  }
+
+  switch (number) {
+    case 1:
+      console.log("one");
+      break;
+    case 2:
+      console.log("two");
+      break;
+    case 3:
+      console.log("three");
+      break;
+    default:
+      console.log("others");
+  }
+
+  rl.close();
+};
+
+rl.question("入力して下さい>", processInput);
