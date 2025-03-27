a8361521c44db418909bb3a32494d1cce69fe6d3
hiro266 <t1freejob@gmail.com>
2025-03-28 01:39:25 +0900

    No. 33 入力値抜き

---
+++ beginner/no.33-display-without-input-value.js
@@ -0,0 +1,32 @@
+// No. 33 入力値抜き
+// 整数値を入力させ、1から9まで、入力値以外を表示するプログラムを作成せよ。
+
+const { rl, isNotInteger } = require("../utils/utils");
+
+rl.prompt();
+
+const processInput = (input) => {
+  const LOWER_LIMIT = 1;
+  const UPPER_LIMIT = 9;
+  const number = Number(input.trim());
+
+  if (isNotInteger(number)) {
+    console.log("整数を入力して下さい");
+    rl.prompt();
+    return;
+  }
+
+  if (number <= LOWER_LIMIT || number > UPPER_LIMIT) {
+    console.log("1~9までの整数を入力して下さい");
+    rl.prompt();
+    return;
+  }
+
+  for (i = 1; i <= UPPER_LIMIT; i++) {
+    if (i !== number) console.log(i);
+  }
+
+  rl.close();
+};
+
+rl.on("line", processInput);
