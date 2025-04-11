12c714792854be16886828afa59704fec7a22442
hiro266 <t1freejob@gmail.com>
2025-04-11 22:37:50 +0900

    No. 87 運命数

---
+++ advance/no.87-destiny-number.js
@@ -0,0 +1,50 @@
+// No. 87 運命数 ☆☆
+// カバラ数秘術という簡単な占いがある。
+// 誕生日を年（西暦）・月・日で表し、それぞれの数字を足し合わせる。
+// 合計した数字が10以上であれば、再びすべての桁の数字を足し合わせる。
+// これを1桁の数字になるまで繰り返し、得られた数字を運命数とする。
+// ただし、計算途中で11、22、33のようにゾロ目の数字になった場合は、それを運命数とする。
+// 例：2015年12月23日→2+0+1+5+1+2+2+3=16→1+6=7
+// 運命数を計算するプログラムを作成せよ。
+
+const { rl } = require("../utils/utils");
+
+rl.setPrompt("誕生日をYYYYMMDDの形式で入力してください");
+rl.prompt();
+
+const calc = (birthday) => {
+  return String(birthday)
+    .split("")
+    .map((value) => Number(value))
+    .reduce((acc, current) => {
+      return acc + current;
+    });
+};
+
+const isRepdigit = (sumCopy) => {
+  return String(sumCopy)
+    .split("")
+    .reduce((acc, current) => {
+      return acc === current;
+    });
+};
+
+const processInput = (birthday) => {
+  if (birthday.length !== 8) {
+    console.log("YYYYMMDD形式で入力して下さい");
+    rl.prompt();
+    return;
+  }
+  const sum = calc(birthday);
+  let sumCopy = sum;
+
+  while (sumCopy > 10) {
+    if (isRepdigit(sumCopy)) break;
+    sumCopy = calc(sumCopy);
+  }
+
+  console.log(sumCopy);
+  rl.close();
+};
+
+rl.on("line", processInput);
